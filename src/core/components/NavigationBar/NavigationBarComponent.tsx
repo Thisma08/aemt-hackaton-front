df8f19c12d3397882897186f738ccec710d5b679
Mathis <mathis.mahaux@gmail.com>
2025-01-20 21:26:22 +0100

    adds css for navbar and budget creation form

--- src/core/components/NavigationBar/NavigationBarComponent.tsx
+++ src/core/components/NavigationBar/NavigationBarComponent.tsx
@@ -5,5 +5,9 @@ export function NavigationBarComponent() {
     return <nav className={"main-navigation-bar"}>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/budgets"}>Budgets</NavLink>
+        <NavLink to={"/"}>Categories</NavLink>
+        <NavLink to={"/"}>Transactions</NavLink>
+        <NavLink to={"/"}>Statistiques</NavLink>
+        <NavLink to={"/"}>Se connecter/Changer de compte</NavLink>
     </nav>
 }
--- src/features/budget/components/CreateBudget/FormBudgetComponent.tsx
+++ src/features/budget/components/CreateBudget/FormBudgetComponent.tsx
@@ -1,46 +1,45 @@
 import {Budget} from "../../types/budget.ts";
 import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import {CreateBudgetOutput} from "../../types/CreateBudgetOutput.ts";
+import "./FormBudgetComponent.css"
 
 export interface BudgetFormComponentProps{
     onBudgetCreation: (budget: Budget) => void
 }
 
-export function FormBudgetComponent({onBudgetCreation}: BudgetFormComponentProps){
-    const [formValidation,setFormValidation] = useState<boolean>(false);
-    const [inputs,setInputs] = useState<CreateBudgetOutput>({
+export function FormBudgetComponent({onBudgetCreation}: BudgetFormComponentProps) {
+    const [formValidation, setFormValidation] = useState<boolean>(false);
+    const [inputs, setInputs] = useState<CreateBudgetOutput>({
         budget: 500,
         month: 1,
         year: new Date().getFullYear()
     });
 
     function checkFormValidity() {
-        if(inputs.budget >= 500){
+        if (inputs.budget >= 500) {
             console.log("Budget is right");
-            if(inputs.year >= 1980 && inputs.year <= new Date().getFullYear()+1){
+            if (inputs.year >= 1980 && inputs.year <= new Date().getFullYear() + 1) {
                 console.log("Year is right");
                 setFormValidation(true);
-            }
-            else
+            } else
                 setFormValidation(false);
-        }
-        else
+        } else
             setFormValidation(false);
     }
 
     useEffect(() => {
         checkFormValidity();
-    },[inputs])
+    }, [inputs])
 
-    function handleChange(event: ChangeEvent<HTMLInputElement|HTMLSelectElement>){
-        const { name, value } = event.target;
+    function handleChange(event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+        const {name, value} = event.target;
         setInputs((values) => ({
             ...values,
             [name]: parseInt(value, 10),
         }));
     }
 
-    function handleSubmit(e: FormEvent){
+    function handleSubmit(e: FormEvent) {
         e.preventDefault();
         onBudgetCreation({
             id: 0,
@@ -69,37 +68,39 @@ export function FormBudgetComponent({onBudgetCreation}: BudgetFormComponentProps
     ]
 
     const years = Array.from(
-        {length: new Date().getFullYear()-2001+2},
-    (_, i) => 2001 + i);
+        {length: new Date().getFullYear() - 2001 + 2},
+        (_, i) => 2001 + i);
 
-    return <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="">
+    return <div className="formWrapper">
+        <form onSubmit={handleSubmit} className={"addBudgetForm"}>
+            <div>
+                <label htmlFor="budget">Budget:</label>
                 <input type="number" min={"500"} name={"budget"} value={inputs.budget} onChange={handleChange}/>
-            </label>
-        </div>
-        <div>
-            <label htmlFor="">
+                <label> €</label>
+            </div>
+            <div>
+                <label htmlFor="month">Mois:</label>
                 <select name="month" value={inputs.month} onChange={handleChange}>
                     {
-                        monthNames.map((monthName,index) =>
-                            <option key={monthName} value={index+1}>{monthName}</option>
+                        monthNames.map((monthName, index) =>
+                            <option key={monthName} value={index + 1}>{monthName}</option>
                         )
                     }
                 </select>
-            </label>
-        </div>
-        <div>
-            <label htmlFor="">
+            </div>
+            <div>
+                <label htmlFor="month">Année:</label>
                 <select name="year" value={inputs.year} onChange={handleChange}>
                     {
                         years.map((year) =>
-                            <option key={"year_"+year} value={year}>{year}</option>
+                            <option key={"year_" + year} value={year}>{year}</option>
                         )
                     }
                 </select>
-            </label>
-        </div>
-        <input type="submit" disabled={!formValidation}/>
-    </form>
+            </div>
+            <div className="submitContainer">
+                <input type="submit" disabled={!formValidation}/>
+            </div>
+        </form>
+    </div>
 }
