1c6ed6a028fb16b64886972fd6ad0f787e5a4123
Mathis <mathis.mahaux@gmail.com>
2025-01-23 19:14:48 +0100

    Upgrade home component

--- src/core/components/Home/HomeComponent.tsx
+++ src/core/components/Home/HomeComponent.tsx
@@ -1,9 +1,25 @@
 import "./HomeComponent.css"
 
 export function HomeComponent(){
-    return <div className={"titleContainer"}>
-        <img src={"tibiscuit.png"} alt={"tibiscuit"}/>
-        <h1>Bienvenue sur Christmas Wallet!</h1>
-        <img src={"tibiscuit.png"} alt={"tibiscuit"}/>
-    </div>
+    return <>
+        <div className={"titleContainer"}>
+            <img src={"tibiscuit.png"} alt={"tibiscuit"}/>
+            <h1>Bienvenue sur Christmas Wallet!</h1>
+            <img src={"tibiscuit.png"} alt={"tibiscuit"}/>
+        </div>
+
+        <div className={"walletImageContainer"}>
+            <img className={"walletImage"} src={"wallet.png"} alt={"wallet"}/>
+        </div>
+
+        <div className={"descContainer"}>
+            <div className={"desc"}>
+                Dans cette application de gestion de budget mensuel, vous pouvez fixer vous-même vos propres budgets
+                mensuels et garder un oeil sur les transactions que vous faites, ainsi que consulter des statistiques sur
+                chaque budget!
+            </div>
+            <br/>
+            <span className={"merryChristmas"}>Joyeux Noël!</span>
+        </div>
+    </>
 }
