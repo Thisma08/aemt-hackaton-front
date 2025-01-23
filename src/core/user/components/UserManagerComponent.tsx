7ddb9de448ce47c2f469106a564c513a7f1924fe
NextAndCie <117162264+NextAndCie@users.noreply.github.com>
2025-01-23 17:49:25 +0100

    Added service to Login and Register, page change to disconnect button when connected

--- src/core/user/components/UserManagerComponent.tsx
+++ src/core/user/components/UserManagerComponent.tsx
@@ -2,18 +2,58 @@ import {LoginComponent} from "./login/LoginComponent.tsx";
 import {RegisterComponent} from "./register/RegisterComponent.tsx";
 import './UserManagerComponent.css'
 import {UserQuery} from "../types/UserQuery.ts";
+import {useEffect, useState} from "react";
+import {getAuthToken, getCurrentUser, login, logout, register} from "../services/user-service.ts";
+import {toast} from "react-toastify";
+import {User} from "../types/User.ts";
 
 export function UserManagerComponent() {
+    const [token, setToken] = useState<string>();
+    const [user, setUser] = useState<User>()
+    const [isConnected, setIsConnected] = useState<boolean>(false);
     const handleLogin = (user: UserQuery) => {
-        console.log(user);
+        const sendLogin = async () => {
+            const jwtToken = await login(user)
+            setToken(jwtToken);
+        }
+        sendLogin()
     }
+
+    useEffect(() => {
+        const localToken = getAuthToken();
+        if(localToken!=null){
+            setToken(localToken)
+        }
+    }, []);
+    useEffect(() => {
+        if (token != "" && token != undefined) {
+            console.log(getCurrentUser());
+            setIsConnected(true);
+        }
+        else setIsConnected(false);
+    }, [token]);
 
     const handleRegister = (user: UserQuery) => {
-        console.log(user);
+        const sendRegister = async () => {
+            const registered = await register(user);
+            if (registered) toast("Utilisateur créé!");
+        }
+        sendRegister()
     }
 
-    return <div>
+    const handleLogout = () => {
+        logout()
+        setToken(undefined);
+    }
+
+    const login_register = <div>
         <LoginComponent onLogin={handleLogin}/>
         <RegisterComponent onRegister={handleRegister}/>
     </div>
+
+    const logout_button = <div className={"disconnectWrapper"}>
+        <button onClick={handleLogout}>Se déconnecter</button>
+    </div>
+
+    return isConnected ? logout_button : login_register;
 }
--- src/core/user/context/AuthContext.tsx
+++
@@ -1,9 +0,0 @@
-import {UserQuery} from "../types/UserQuery.ts";
-//import {createContext} from "react";
-
-export interface Action{
-    type: string;
-    user: UserQuery;
-}
-
-//export const AuthContext = createContext<User>(undefined);
